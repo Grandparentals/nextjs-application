ff5dfc2d78412254d76f3da6255eefe0a7dbd140
Wiktor Matecki <wmatecki97@gmail.com>
2022-11-19 20:11:49 +0100

    Footer removed

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -154,7 +154,7 @@ function Navbar({page}: {page: string}) {
                                                     'px-3 group bg-white rounded-md inline-flex items-center text-base font-medium border-2 border-transparent hover:text-gray-900 focus:text-white focus:bg-teal-600 focus:outline-none focus:border-teal-600 focus:rounded-md'
                                                 )}
                                             >
-                                                <span>TODO category 2</span>
+                                                <span>Types of help</span>
                                                 <ChevronDownIcon
                                                     className={classNames(
                                                         open ? 'text-gray-600' : 'text-gray-400',
@@ -199,21 +199,6 @@ function Navbar({page}: {page: string}) {
                                                                 </div>
                                                             </a>
                                                         ))}
-                                                    </div>
-                                                    <div className="bg-gray-50">
-                                                        <div className="max-w-7xl mx-auto space-y-6 px-4 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-6 lg:px-8">
-                                                            {callsToAction.map((item) => (
-                                                                <div key={item.name} className="flow-root">
-                                                                    <a
-                                                                        href={item.href}
-                                                                        className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
-                                                                    >
-                                                                        <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                                                        <span className="ml-3">{item.name}</span>
-                                                                    </a>
-                                                                </div>
-                                                            ))}
-                                                        </div>
                                                     </div>
                                                 </Popover.Panel>
                                             </Transition>
