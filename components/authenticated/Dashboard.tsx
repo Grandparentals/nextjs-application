b0023eef8a5faccf2d725c51077304f31d003fdc
Marcus Bego Gomes <3407949+viniciusgomes@users.noreply.github.com>
2022-11-19 22:53:58 +0000

    feat: creating a configuration for spciality (#7)
    Co-authored-by: M.V. Bego <viniciusgomes@users.noreply.github.com>

--- components/authenticated/Dashboard.tsx
+++ components/authenticated/Dashboard.tsx
@@ -153,7 +153,7 @@ function Dashboard() {
                                             <a
                                                 className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50"
                                             >
-                                                View profile
+                                                Edit profile
                                             </a>
                                         </Link>
                                     </div>
