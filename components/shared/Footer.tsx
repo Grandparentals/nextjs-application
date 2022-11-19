b5183db4e20eb23fbca69d9d8ea1374d7485008f
Wiktor Matecki <wmatecki97@gmail.com>
2022-11-19 20:21:23 +0100

    build fix

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -42,14 +42,6 @@ function Footer() {
             </h2>
             <div className="mx-auto max-w-7xl px-4 pt-16 pb-8 sm:px-6 lg:px-8 lg:pt-24">
                 <div className="mt-12 border-t border-gray-200 pt-8 md:flex md:items-center md:justify-between lg:mt-16">
-                    <div className="flex space-x-6 md:order-2">
-                        {footerNavigation.social.map((item) => (
-                            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-                                <span className="sr-only">{item.name}</span>
-                                <item.icon className="h-6 w-6" aria-hidden="true" />
-                            </a>
-                        ))}
-                    </div>
                     <p className="mt-8 text-base text-gray-400 md:order-1 md:mt-0">
                         &copy; 2022 GrandpaRental. All rights reserved.
                     </p>
