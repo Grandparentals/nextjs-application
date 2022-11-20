8f333a2334da8d5c25b249528b426f9d877d8ae6
Wiktor Matecki <wmatecki97@gmail.com>
2022-11-20 05:18:50 +0100

    articles on the main page

--- components/shared/Features.tsx
+++ components/shared/Features.tsx
@@ -10,11 +10,7 @@ function Features() {
                 <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
                     <div className="mx-auto max-w-xl px-4 sm:px-6 lg:mx-0 lg:max-w-none lg:py-16 lg:px-0">
                         <div>
-                            <div>
-                                <span className="flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-r from-teal-600 to-teal-600">
-                                    <InboxIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                                </span>
-                            </div>
+                           
                             <div className="mt-6">
                                 <h2 className="text-3xl font-bold tracking-tight text-gray-900">The second start</h2>
                                 <p className="mt-4 text-lg text-gray-500">
@@ -38,8 +34,10 @@ function Features() {
                             <blockquote>
                                 <div>
                                     <p className="text-base text-gray-500">
-                                        &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean
-                                        curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;
+                                        &ldquo;I wish that my grandpa have some goal in his life. 
+                                        After the grandkids grow up and didn't have so many time to spend with him his life slowed up a lot.
+                                        This caused lonliness and even the problems with mobility. Now famili visits are almost the only joy for him and we wish for him to be active in social life again.
+                                        &rdquo;
                                     </p>
                                 </div>
                                 <footer className="mt-3">
@@ -52,7 +50,7 @@ function Features() {
                                             />
                                         </div>
                                         <div className="text-base font-medium text-gray-700">
-                                            Marcia Hill, CEO Medical Center Manager
+                                            Zofia, grandchild
                                         </div>
                                     </div>
                                 </footer>
@@ -63,8 +61,8 @@ function Features() {
                         <div className="-mr-48 pl-4 sm:pl-6 md:-mr-16 lg:relative lg:m-0 lg:h-full lg:px-0">
                             <img
                                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
-                                alt="Inbox user interface"
+                                src="https://images.unsplash.com/photo-1613147474738-b988fa947e40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+                                alt="Second start"
                             />
                         </div>
                     </div>
@@ -74,20 +72,16 @@ function Features() {
                 <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-2 lg:gap-24 lg:px-8">
                     <div className="mx-auto max-w-xl px-4 sm:px-6 lg:col-start-2 lg:mx-0 lg:max-w-none lg:py-32 lg:px-0">
                         <div>
-                            <div>
-                                <span className="flex h-12 w-12 items-center justify-center rounded-md bg-gradient-to-r from-teal-600 to-teal-600">
-                                    <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
-                                </span>
-                            </div>
+                            
                             <div className="mt-6">
                                 <h2 className="text-3xl font-bold tracking-tight text-gray-900">
-                                   Melhor entendimento de seus Pacientes
+                                   Create social bonds
                                 </h2>
                                 <p className="mt-4 text-lg text-gray-500">
-                                    Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                                    porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at.
-                                    Lectus viverra dui tellus ornare pharetra.
-                                </p>
+                                    It is never too late to make new friendships and create new relations. <br/>
+                                    We believe we are able to change the way that people are co-existing in the society by helping them to create social bond with people who live close.<br/>
+                                    The number of possible types of collaboration between the people is infinite and the only thing limiting us is our own imagination. <br/>
+                                    Join the platform to start a new jurney in your life.                               </p>
                                 <div className="mt-6">
                                     <a
                                         href="#"
@@ -103,7 +97,7 @@ function Features() {
                         <div className="-ml-48 pr-4 sm:pr-6 md:-ml-16 lg:relative lg:m-0 lg:h-full lg:px-0">
                             <img
                                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                                src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                                src="https://images.unsplash.com/photo-1494438043283-22a3c46831a4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
                                 alt="Customer profile user interface"
                             />
                         </div>
