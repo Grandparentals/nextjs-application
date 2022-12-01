e3317fd20d9b41d2012173f668686937349ae3f7
M.V. Bego <viniciusgomes@users.noreply.github.com>
2022-12-01 15:14:19 +0100

    feat: create locale configuration and english file

---
+++ locales/en-EN.ts
@@ -0,0 +1,25 @@
+export default {
+    translation: {
+        common: {
+            remoteuOrg: 'Remoteu.org',
+            save: 'Salvar',
+            edit: 'Editar',
+            continue: 'Continuar',
+            ok: 'Ok',
+            next: 'Próximo',
+            previous: 'Anterior',
+            about: 'Sobre',
+            knowMore: 'Saiba mais',
+            signUp: 'Sign up'
+        },
+        navBar: {
+            devs: 'Devs',
+            companies: 'Empresas'
+        },
+        login: {
+            login: 'Login',
+            logout: 'Logout',
+            google: 'Login com Google'
+        }
+    }
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../context/AuthContext'
+import '../locales'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <AuthProvider><Component {...pageProps} /></AuthProvider>
