2e766e336f4e41b35077f40b9628ffc2761cd4ee
wmatecki97 <wmatecki97@gmail.com>
2022-12-19 20:02:39 +0000

    polish translations added

--- locales/translations/pl-PL.ts
+++ locales/translations/pl-PL.ts
@@ -1,29 +1,115 @@
 export default {
     translation: {
         common: {
-            grandparentals: 'GrandpaRentals'
+            grandparentals: 'GrandpaRentals',
+            rights: '2022 GrandpaRental. Wszelkie prawa zastrzeżone.',
         },
         navBar: {
-            dashboard: 'Dashboard',
-            discoverPeople: 'Discover people',
-            typesOfHelp: 'Types of help',
-            aboutUs: "About Us",
-            howItWorks: "How it Works"
+            dashboard: 'Kokpit',
+            discoverPeople: 'Odkryj ludzi',
+            typesOfHelp: 'Rodzaje pomocy',
+            aboutUs: 'O nas',
+            howItWorks: 'Jak to działa',
+            solutions: [
+                {
+                    name: 'Porozmawiajmy',
+                    description: 'Chcesz po prostu znaleźć przyjaciela, z którym mogłbyś spotykać się od czasu do czasu na rozmowę i kawę? Jest wielu ludzi, którzy chętnie z tobą porozmawiają.',
+                },
+                {
+                    name: 'Zjedzmy razem',
+                    description: 'Jeśli nie lubisz jeść samemu lub potrzebujesz pomocy w przygotowaniu posiłku, spójrz na ludzi, którzy mogą ci pomóc.',
+                },
+                {
+                    name: 'Pomoc z nauką i zadaniem domowym',
+                    description: 'Jeśli twoje dzieci potrzebują kogoś, kto pomoże im z zadaniem domowym, możesz szukać ludzi, którzy mogą im w tym pomóc.',
+                },
+                {
+                    name: 'Opieka nad zwierzętami',
+                    description: 'Znajdź ludzi, którzy mogą ci pomóc w opiece nad zwierzętami, gdy planujesz wyjazd na wakacje lub pójdą z tobą na spacer z psem, gdy nie możesz.',
+                },
+            ],
         },
         login: {
-            login: 'Login',
-            logout: 'Logout',
-            google: 'Login with Google'
+            login: 'Zaloguj się',
+            logout: 'Wyloguj się',
+            google: 'Zaloguj się przez Google',
         },
         page: {
             aboutUs: {
-                title: "About Us",
-                content: "tutaj jest kontent bla bla bla"
+                title: 'O nas',
+                content: 'Oto treść bla bla bla',
             },
             howItWorks: {
-                title: "How it Works",
-                content: "Tutaj jest kontent bla bla bla"
-            }
-        }
-    }
-};
+                title: 'Jak to działa',
+                content: 'Oto treść bla bla bla',
+            },
+            discoverPeople: {
+                head: {
+                    title: 'Odkryj ludzi - Grandparentals',
+                },
+                title: 'Ludzie',
+                appliedOn: 'Zastosowano dnia',
+            },
+            personalInformation: {
+                head: {
+                    title: 'Informacje osobiste',
+                },
+                title: 'Informacje osobiste',
+                joinedOn: 'Dołączył dnia',
+                personalInformation: 'Informacje osobiste',
+                about: 'O mnie',
+                professionalSummary: 'Podsumowanie zawodowe',
+                telephone: 'Telefon',
+                location: 'Lokalizacja',
+            },
+            userDashboard: {
+                title: 'Profil',
+                subtitle: 'Przegląd profilu',
+                welcome: 'Witaj ponownie,',
+            },
+        },
+        components: {
+            features: {
+                icons: {
+                    one: {
+                        name: 'Zwróć uwagę na osobę potrzebującą',
+                        description: 'Pierwszą rzeczą, która przyniesie zmianę, jest zauważenie potrzeby i rozmowa z osobą.',
+                    },
+                    two: {
+                        name: 'Zarejestruj osobę',
+                        description: 'Po zalogowaniu się do systemu możesz utworzyć profil osoby, która chce dzielić się swoim czasem i talentami z innymi. Profil zawiera informacje osobiste o osobie, jej umiejętnościach i celach.',
+                    },
+                    three: {
+                        name: 'Pozwól innym kontaktować się z tą osobą',
+                        description: 'Utworzony profil będzie dostępny publicznie, więc osoba, która potrzebowałaby pomocy, będzie mogła znaleźć ludzi, którzy jej pomogą lub po prostu porozmawiają i spędzą razem czas.',
+                    },
+                    four: {
+                        name: 'Utwórz nowe relacje',
+                        description: 'Gdy ludzie mogą się znaleźć, wierzymy, że jesteśmy w stanie wprowadzić nowe ludzkie relacje do społeczeństwa.',
+                    },
+                },
+                links: {
+                    one: {
+                        name: 'Znajdź osobę',
+                        description: 'Możesz przeglądać profile innych osób i wyszukiwać według lokalizacji lub rodzaju pomocy, jakiej szukają.',
+                    },
+                    two: {
+                        name: 'Kontakt',
+                        two: {
+                            name: 'Kontakt',
+                            description: 'Po znalezieniu odpowiedniej osoby możesz skontaktować się z nią za pośrednictwem wiadomości na stronie lub poza nią.',
+                        },
+                        three: {
+                            name: 'Umów się',
+                            description: 'Po uzgodnieniu szczegółów możesz umówić się na spotkanie z osobą, która ci pomoże.',
+                        },
+                        four: {
+                            name: 'Oceń swoje doświadczenie',
+                            description: 'Po zakończeniu współpracy możesz ocenić swoje doświadczenie z tą osobą i pomóc innym wybrać odpowiednią osobę.',
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
